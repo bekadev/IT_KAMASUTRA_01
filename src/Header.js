b3b6800dc71fd3b71ddde10fb009bc59bac3633f
beka <bigsamsa3@mail.ru>
2022-05-19 20:25:16 +0600

    01

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div>
+            <ul>
+                <li>skill</li>
+                <li>or</li>
+                <li>money</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Header;
